3c454fb96b87be3a3134dfbb2d7e3ea1e2dcad3c
MichaelaKemp <231001@virtualwindow.co.za>
2023-05-20 18:32:46 +0200

    Week 2 homework
    Week 2 homework was completed.

---
+++ js/index.js
@@ -0,0 +1,41 @@
+let firstName = "Michaela"
+let surname = "Kemp"
+let age = 20
+const subject = "Development"
+
+let message = "Hello, my name is " + firstName + " " + surname + "," + " I am " + age + " years old and I am studying " + subject
+
+console.log(message)
+
+let numOne = 23
+let numTwo = 69
+let numThree = 113
+let numFour = 42
+let answer = 0
+
+answer = numOne + numTwo / numThree * numFour
+
+console.log(answer)
+
+let timmy = 10
+let susan = 25
+let value = (0.15 * timmy) + (0.26 * susan)
+
+value = "The Tax Man will claim " + value + " from Timmy and Susan"
+
+console.log(value)
+
+let john = 45
+let tim = 62
+let alfie = 23
+let gwen = 83
+
+john = john + 7
+tim = tim + 7
+alfie = alfie + 7
+gwen = gwen + 7
+
+value = (john + tim + alfie + gwen) / 4
+
+console.log(value)
+
